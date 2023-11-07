fc9440fe1f950616234358257280c36bd4d67eed
Julia Miklosovicova <miklosovicl@gmail.com>
2023-11-07 23:21:15 +0100

    removed hibernate configuration file and done it in code instead because it was causing errors

--- src/main/java/org/example/di/MainModule.java
+++ src/main/java/org/example/di/MainModule.java
@@ -7,7 +7,12 @@ import org.example.data.data_source.UserDaoImpl;
 import org.example.data.repository.UserRepositoryImpl;
 import org.example.domain.repository.UserRepository;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.LinkedList;
 
 public class MainModule extends AbstractModule {
 
@@ -19,8 +24,29 @@ public class MainModule extends AbstractModule {
     }
 
     @Provides
-    public Configuration provideConfiguration() {
-        return new Configuration().configure("hibernate.cfg.xml");
+    public Configuration provideHibernateConfiguration() {
+        Configuration configuration = new Configuration();
+        // Database connection settings for H2
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:testdb"); // In-memory H2 database
+
+        // Entity mappings
+        configuration.addAnnotatedClass(User.class); // Add your User entity class
+
+        // Other Hibernate properties
+        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+        configuration.setProperty("hibernate.show_sql", "false");
+
+        // Create a ServiceRegistry and MetadataSources
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build();
+
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        metadataSources.addAnnotatedClass(User.class); // Add your User entity class
+
+        return configuration;
     }
 
     @Provides
